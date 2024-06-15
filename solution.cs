f070ebf4506ca5a04ceaff169a716e3562a9fffd
Kooreis <kooreis.official@gmail.com>
2024-06-15 18:32:40 -0500

    Added a 'scheduleJob' method to the JobScheduler class. This method takes a job (a function) and a delay (in milliseconds), and schedules the job to be executed after the delay using setTimeout. The timeoutId returned by setTimeout is stored along with the job in the 'jobs' array.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,7 @@
-class JobScheduler {
-    constructor() {
-        this.jobs = [];
+scheduleJob(job, delay) {
+        const timeoutId = setTimeout(() => {
+            job();
+            this.jobs = this.jobs.filter(j => j.id !== timeoutId);
+        }, delay);
+        this.jobs.push({ id: timeoutId, job });
     }
-}
